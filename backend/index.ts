4e5d6e7e33d48179382bcea9e56e0740b1e8d34f
Bonioszko <bartek.pers@gmail.com>
2024-04-03 20:15:46 +0200

    preproduction

--- backend/index.ts
+++ backend/index.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response, Application } from "express";
 import dotenv from "dotenv";
 const mongoose = require("mongoose");
+const path = require("path");
 const cookieParser = require("cookie-parser");
 import User from "./models/user";
 
@@ -10,7 +11,9 @@ mongoose.connect(process.env.MONGODB_KEY);
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "mongo connection error"));
 
+const currentDirectory = process.cwd();
 const app: Application = express();
+app.use(express.static(path.join(currentDirectory, "/frontend/dist")));
 const port = process.env.PORT || 8000;
 app.use(express.json()); // for parsing application/json
 app.use(cookieParser());
@@ -25,4 +28,8 @@ app.use("/api/apartment", require("./routes/apartmentRouter"));
 
 app.listen(port, () => {
     console.log(`Server is Fire at http://localhost:${port}`);
+});
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(currentDirectory, "frontend", "dist", "index.html"));
 });
